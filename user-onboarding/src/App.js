f327432c59b4de80cea3dd49ac066be1469fffe4
Nathaniel Patterson <odst0016@gmail.com>
2020-09-16 16:14:44 -0400

    mvp done need to practice more with forms

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import Form from "./Form";
+import "./App.css";
+import schema from "./schema";
+
+const initialForm = {
+  name: "",
+  email: "",
+  password: "",
+  terms: false,
+};
+const initialErrors = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 function App() {
+  const [formState, setFormState] = useState(initialForm);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [errors, setErrors] = useState(initialErrors);
+  const [post, setPost] = useState([]);
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then((res) => {
+        setPost(res.data); // get just the form data from the REST api
+        console.log("success", post);
+        // reset form if successful
+        setFormState({
+          name: "",
+          email: "",
+          terms: "",
+          positions: "",
+          motivation: "",
+        });
+      })
+      .catch((err) => console.log(err.response));
+  };
+
+  const validateChange = (e) => {
+    // Reach will allow us to "reach" into the schema and test only one part.
+    yup
+      .reach(schema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0],
+        });
+      });
+  };
+
+  const inputChange = (e) => {
+    e.persist();
+    const newUser = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    };
+    validateChange(e);
+    setFormState(newUser);
+  };
+
+  useEffect(() => {
+    schema.isValid(formState).then((valid) => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        buttonDisabled={buttonDisabled}
+        inputChange={inputChange}
+        formState={formState}
+        onSubmit={formSubmit}
+        errors={errors}
+      />
+      <pre>{JSON.stringify(post, null, 2)}</pre>
     </div>
   );
 }
